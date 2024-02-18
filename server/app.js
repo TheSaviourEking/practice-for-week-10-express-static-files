618791f853f261a6095d588643e92777ac0d9bdd
TheSaviourEking <saviour.eking@outlook.com>
2024-02-18 04:27:33 +0100

    chore: implement project solutions

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 // Your code here
+app.use(express.static('assets'));
+
+const path = require('path');
+app.use('/scripts', express.static(path.join(__dirname, 'assets', 'scripts')));
+
+app.use('/stylesheets', express.static(path.join(__dirname, 'assets', 'css')));
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
