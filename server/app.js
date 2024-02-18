5214756c25b0c033a0c1d8ef76a62c1e3f85e5c6
TheSaviourEking <saviour.eking@outlook.com>
2024-02-18 04:33:52 +0100

    chore: implement project solutions

--- server/app.js
+++ server/app.js
@@ -1,12 +1,20 @@
+const exp = require('constants');
 const express = require('express');
 const app = express();
 // Your code here
+
+// Part 1: File paths appended to root URL
 app.use(express.static('assets'));
 
+// Part 2: File paths appended to root URL II
 const path = require('path');
 app.use('/scripts', express.static(path.join(__dirname, 'assets', 'scripts')));
 
+// Part 3: File paths appended to a resource
 app.use('/stylesheets', express.static(path.join(__dirname, 'assets', 'css')));
+
+// File paths appended to a resource II
+app.use('/stickers', express.static(path.join(__dirname, 'assets', 'images')))
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
